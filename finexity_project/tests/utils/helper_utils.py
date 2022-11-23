4528ef39c254d80c3f558d07b4659c73ec3f793f
Deepti Chouhan <deepti chouhan>
2022-11-23 14:20:22 +0530

    utils file

---
+++ finexity_project/tests/utils/helper_utils.py
@@ -0,0 +1,30 @@
+import time
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+class Helper():
+    def __init__(self, context):
+        self.context = context
+        
+    def explicit_wait(self,locator_time):
+        wait = WebDriverWait(self.context.browser, 25).until(
+            EC.visibility_of_all_elements_located(locator_time))
+        for locator in wait:
+            return locator
+            
+    def click_on_multiple_element(self,locator):
+        elements = [element for element in WebDriverWait(self.context.browser, 15).until(
+            EC.visibility_of_all_elements_located(locator))]
+        for element in elements:
+            element.click()
+            print("clicked on " , element.text)
+        
+    def hoverover(self,hoverover_on_locator):
+        wait = WebDriverWait(self.context.browser,25)
+        hover_bttn = wait.until(EC.element_to_be_clickable(hoverover_on_locator))
+        actionchains=ActionChains(self.context.browser)
+        actionchains.move_to_element(hover_bttn).perform()
+        time.sleep(5)    
+                   
+                   
+    
